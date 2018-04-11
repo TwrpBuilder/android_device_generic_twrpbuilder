4f5f0c2f020c7ecda8b4f2decff7475e1da46a61
androidlover5842 <androidlover5842@gmail.com>
2018-04-11 09:02:15 +0530

    common: add support for landscape devices

--- BoardConfig32.mk
+++ BoardConfig32.mk
@@ -21,12 +21,16 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 ifneq ($(filter msm8974 msm8227 msm8627 msm8230 apq8030 msm8630 msm8930 msm8930AA apq8060A msm8260A msm8660A msm8960 MPQ8064 msm8674 msm8274 apq8084,$(TARGET_BOARD_PLATFORM)),)
 TARGET_CPU_VARIANT := krait
+TARGET_RECOVERY_QCOM_RTC_FIX := true
 else 
 TARGET_CPU_VARIANT := cortex-a9
 endif
 
 # TWRP
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+ifneq ($(filter ,$(TW_THEME)),)
 TW_THEME := portrait_hdpi
+endif
 TW_INCLUDE_CRYPTO := true
 RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
--- seEnforcing.mk
+++ seEnforcing.mk
@@ -1,12 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-## Don't change anything under here. The variables are named MSM8916_whatever
-## on purpose, to avoid conflicts with similarly named variables at other
-## parts of the build environment
-
-## Imported from the original makefile...
-
-## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
